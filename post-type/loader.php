ad15fedc8c2e18ed159b6ab37a840ea97ab8a5b6
corsac <corsacca@gmail.com>
2021-08-31 14:00:37 -0700

    phpcs

--- post-type/loader.php
+++ post-type/loader.php
@@ -32,12 +32,12 @@ add_filter( 'dt_post_type_modules', function( $modules ){
         "description" => "Campaigns - 24Hour Prayer"
     ];
 
-//    if ( isset( $modules["contacts_base"] ) ){
-//        $modules["contacts_base"]["locked"] = false;
-//    }
-//    if ( isset( $modules["groups_base"] ) ){
-//        $modules["groups_base"]["locked"] = false;
-//    }
+    if ( isset( $modules["contacts_base"] ) ){
+        $modules["contacts_base"]["locked"] = false;
+    }
+    if ( isset( $modules["groups_base"] ) ){
+        $modules["groups_base"]["locked"] = false;
+    }
 
     return $modules;
 }, 20, 1 );
@@ -58,4 +58,10 @@ DT_Campaign_24Hour_Prayer::instance();
 new DT_Prayer_Campaign_24_Hour_Magic_Link();
 
 require_once 'dt-subscriptions.php';
+
+require_once 'module-campaigns-year.php';
+DT_Campaign_Year::instance();
+require_once plugin_dir_path( __DIR__ ) . '/magic-links/year/year-campaign.php';
+new DT_Prayer_Campaign_Year_Magic_Link();
+
 
