dc86e4153e70d8b128fa64e0d6a22b3bda9b24f9
corsacca <corsacca@gmail.com>
2021-11-12 14:35:19 +0100

    Don't enqueue script multiple times

--- magic-links/24hour.php
+++ magic-links/24hour.php
@@ -2,38 +2,44 @@
 
 function dt_24hour_campaign_register_scripts( $atts ){
     //campaigns core js
-    wp_enqueue_script( 'dt_campaign_core', trailingslashit( plugin_dir_url( __FILE__ ) ) . '../post-type/campaign_core.js', [
-        'jquery',
-        'lodash'
-    ], filemtime( plugin_dir_path( __FILE__ ) . '../post-type/campaign_core.js' ), true );
-    wp_localize_script( 'dt_campaign_core', 'dt_campaign_core', [ 'color' => $atts["color"] ?? '' ] );
+    if ( !wp_script_is('dt_campaign', "registered" ) ){
+        wp_register_script( 'dt_campaign_core', trailingslashit( plugin_dir_url( __FILE__ ) ) . '../post-type/campaign_core.js', [
+            'jquery',
+            'lodash'
+        ], filemtime( plugin_dir_path( __FILE__ ) . '../post-type/campaign_core.js' ), true );
+        wp_localize_script( 'dt_campaign_core', 'dt_campaign_core', [ 'color' => $atts["color"] ?? '' ] );
+    }
 
     wp_enqueue_style( 'dt_campaign_style', trailingslashit( plugin_dir_url( __FILE__ ) ) . '24hour.css', [], filemtime( plugin_dir_path( __FILE__ ) . '24hour.css' ) );
 
     //24 hour campaign js
-    wp_enqueue_script( 'dt_campaign', trailingslashit( plugin_dir_url( __FILE__ ) ) . '24hour.js', array( 'jquery' ), filemtime( plugin_dir_path( __FILE__ ) . '24hour.js' ), true );
-    wp_localize_script(
-        'dt_campaign', 'campaign_objects', [
-            'translations' => [
-                //"campaign_duration" => __( 'Everyday from %1$s to %2$s', "disciple-tools-prayer-campaigns" ),
-                "select_a_time" => __( 'Select a time', 'disciple-tools-prayer-campaigns' ),
-                "fully_covered_once" => __( 'fully covered once', 'disciple-tools-prayer-campaigns' ),
-                "fully_covered_x_times" => __( 'fully covered %1$s times', 'disciple-tools-prayer-campaigns' ),
-                "time_slot_label" => _x( '%1$s for %2$s minutes.', "Monday 5pm for 15 minutes", 'disciple-tools-prayer-campaigns' ),
-                //"going_for_twice" => __( 'All of the %1$s time slots are covered in once prayer once. Help us cover them twice!', 'disciple-tools-prayer-campaigns' ),
-                //"invitation" => __( '%1$s people praying %2$s minutes everyday day will cover the region in 24h prayer.', 'disciple-tools-prayer-campaigns' ),
-            ],
-            "parts" => [
-                "root" => $atts['root'],
-                "type" => $atts['type'],
-                "public_key" => $atts['public_key'],
-                "meta_key" => $atts['meta_key'],
-                "post_id" => $atts["post_id"],
-                "lang" => $atts["lang"] ?? "en_US"
-            ],
-            "root" => $atts["rest_url"]
-        ]
-    );
+    if ( !wp_script_is('dt_campaign' ) ){
+        wp_enqueue_script( 'dt_campaign', trailingslashit( plugin_dir_url( __FILE__ ) ) . '24hour.js', array( 'jquery', 'dt_campaign_core' ), filemtime( plugin_dir_path( __FILE__ ) . '24hour.js' ), true );
+        wp_localize_script(
+            'dt_campaign', 'campaign_objects', [
+                'translations' => [
+                    //"campaign_duration" => __( 'Everyday from %1$s to %2$s', "disciple-tools-prayer-campaigns" ),
+                    "select_a_time" => __( 'Select a time', 'disciple-tools-prayer-campaigns' ),
+                    "fully_covered_once" => __( 'fully covered once', 'disciple-tools-prayer-campaigns' ),
+                    "fully_covered_x_times" => __( 'fully covered %1$s times', 'disciple-tools-prayer-campaigns' ),
+                    "time_slot_label" => _x( '%1$s for %2$s minutes.', "Monday 5pm for 15 minutes", 'disciple-tools-prayer-campaigns' ),
+                    //"going_for_twice" => __( 'All of the %1$s time slots are covered in once prayer once. Help us cover them twice!', 'disciple-tools-prayer-campaigns' ),
+                    //"invitation" => __( '%1$s people praying %2$s minutes everyday day will cover the region in 24h prayer.', 'disciple-tools-prayer-campaigns' ),
+                ],
+                "parts" => [
+                    "root" => $atts['root'],
+                    "type" => $atts['type'],
+                    "public_key" => $atts['public_key'],
+                    "meta_key" => $atts['meta_key'],
+                    "post_id" => $atts["post_id"],
+                    "lang" => $atts["lang"] ?? "en_US"
+                ],
+                "root" => get_rest_url(),
+                "remote" => $atts["rest_url"] !== get_rest_url(),
+            ]
+        );
+    }
+    set_transient( "dt_magic_link_remote_" . $atts["post_id"],  $atts["rest_url"], DAY_IN_SECONDS );
 }
 
 function dt_24hour_campaign_body( $color = "", $section = "" ){
