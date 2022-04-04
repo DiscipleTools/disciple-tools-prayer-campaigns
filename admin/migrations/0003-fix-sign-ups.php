653d9f7f23516e0e55fb6fe06c1aef138567a00b
corsacca <corsacca@gmail.com>
2022-04-04 10:58:51 +0100

    Fix migration typo.

--- admin/migrations/0003-fix-sign-ups.php
+++ admin/migrations/0003-fix-sign-ups.php
@@ -5,11 +5,19 @@ if ( ! defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly
 /**
  * Class DT_Prayer_Campaign_Migration_0001
  */
-class DT_Prayer_Campaign_Migration_0002 extends DT_Prayer_Campaign_Migration {
+class DT_Prayer_Campaign_Migration_0003 extends DT_Prayer_Campaign_Migration {
     /**
      * @throws \Exception  Got error when creating table $name.
      */
     public function up() {
+        global $wpdb;
+        $wpdb->query( "UPDATE $wpdb->dt_reports r
+            set r.value = 1
+            WHERE r.post_type = 'subscriptions'
+            AND r.subtype = '24hour'
+            AND r.timestamp > 1648774861
+            AND r.timestamp < 1648861261
+        ");
     }
 
     /**
