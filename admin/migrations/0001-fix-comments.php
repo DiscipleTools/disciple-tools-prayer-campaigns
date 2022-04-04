711ab316e646e2ac880e3787170fa248bd45296b
corsacca <corsacca@gmail.com>
2022-04-04 12:54:27 +0100

    Empty out migrations

--- admin/migrations/0001-fix-comments.php
+++ admin/migrations/0001-fix-comments.php
@@ -10,13 +10,6 @@ class DT_Prayer_Campaign_Migration_0001 extends DT_Prayer_Campaign_Migration {
      * @throws \Exception  Got error when creating table $name.
      */
     public function up() {
-        global $wpdb;
-        $wpdb->query( "UPDATE $wpdb->postmeta pm
-            JOIN $wpdb->postmeta pm2 ON (pm2.post_ID = pm.post_ID AND pm2.meta_key = 'tags' AND pm2.meta_value = 'sent-pre-signup' )
-            JOIN $wpdb->posts p ON ( p.ID = pm.post_ID AND p.post_type = 'subscriptions' )
-            set pm.meta_value = 1
-            WHERE pm.meta_key = 'receive_prayer_time_notifications'
-        ");
     }
 
     /**
--- admin/migrations/0003-fix-sign-ups.php
+++ admin/migrations/0003-fix-sign-ups.php
@@ -10,14 +10,6 @@ class DT_Prayer_Campaign_Migration_0003 extends DT_Prayer_Campaign_Migration {
      * @throws \Exception  Got error when creating table $name.
      */
     public function up() {
-        global $wpdb;
-        $wpdb->query( "UPDATE $wpdb->dt_reports r
-            set r.value = 1
-            WHERE r.post_type = 'subscriptions'
-            AND r.subtype = '24hour'
-            AND r.timestamp > 1648774861
-            AND r.timestamp < 1648861261
-        ");
     }
 
     /**
