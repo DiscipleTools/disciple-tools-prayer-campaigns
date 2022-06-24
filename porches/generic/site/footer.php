dbe11a4e9fdc7b76443aa1df3f97a691b9c37e0c
Nath <nathinabob@gmail.com>
2022-06-24 10:24:56 +0100

    refactor: move scripts from footer file to enqueued properly

--- porches/generic/site/footer.php
+++ porches/generic/site/footer.php
@@ -50,28 +50,3 @@
         <div class="double-bounce2"></div>
     </div>
 </div>
-
-<!-- jQuery first, then Tether, then Bootstrap JS. -->
-<!-- TODO: put these into enqueue scripts properly -->
-<?php // phpcs:disable ?>
-<script src="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?>js/jquery-min.js"></script>
-<script src="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?>js/tether.min.js"></script>
-<script src="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?>js/bootstrap.min.js"></script>
-<script src="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?>js/classie.js"></script>
-<!--<script src="--><?php //echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?><!--js/mixitup.min.js"></script>-->
-<!--<script src="--><?php //echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?><!--js/nivo-lightbox.js"></script>-->
-<!--<script src="--><?php //echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?><!--js/owl.carousel.min.js"></script>-->
-<script src="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?>js/jquery.stellar.min.js"></script>
-<script src="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?>js/jquery.nav.js"></script>
-<script src="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?>js/smooth-scroll.js"></script>
-<script src="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?>js/smooth-on-scroll.js"></script>
-<script src="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?>js/wow.js"></script>
-<script src="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?>js/menu.js"></script>
-<!--<script src="--><?php //echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?><!--js/jquery.vide.js"></script>-->
-<script src="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?>js/jquery.counterup.min.js"></script>
-<!--<script src="--><?php //echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?><!--js/jquery.magnific-popup.min.js"></script>-->
-<script src="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?>js/waypoints.min.js"></script>
-<!--<script src="--><?php //echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?><!--js/form-validator.min.js"></script>-->
-<!--<script src="--><?php //echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?><!--js/contact-form-script.js"></script>-->
-<script src="<?php echo esc_url( trailingslashit( plugin_dir_url( __FILE__ ) ) ) ?>js/main.js?ver=<?php echo esc_attr( filemtime( trailingslashit( plugin_dir_path( __FILE__ ) ) . 'js/main.js' ) ) ?>"></script>
-<?php // phpcs:enable ?>
