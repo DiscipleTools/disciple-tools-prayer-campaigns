8b1dfcf9d613d02d3df49ae589a66eaef3d3d731
Nath <nathinabob@gmail.com>
2022-07-13 16:33:51 +0100

    feat: proof of concept installing posts from an RSS feed URL

--- admin/dt-porch-admin-tab-starter-content.php
+++ admin/dt-porch-admin-tab-starter-content.php
@@ -222,7 +222,9 @@ class DT_Porch_Admin_Tab_Starter_Content {
 
         $all_good_to_go = false;
         $message = "";
-        $tmp_file = "";
+        $tmp_file_name = "";
+        $invalidurl = false;
+        $feeds = null;
         if ( isset( $_POST['install_from_file_nonce'] ) && wp_verify_nonce( sanitize_text_field( wp_unslash( $_POST['install_from_file_nonce'] ) ), 'install_from_file' ) ) {
 
             $max_file_size = 1024 * 1024 * 5;
@@ -231,31 +233,65 @@ class DT_Porch_Admin_Tab_Starter_Content {
                 $prayer_post_importer->set_append_date( sanitize_text_field( wp_unslash( $_POST['append_date'] ) ) );
             }
 
-            $file = isset( $_FILES['file'] ) ? dt_recursive_sanitize_array( wp_unslash( $_FILES['file'] ) ) : '';
 
-            $tmp_file = $file["tmp_name"];
+            if ( !empty( $_FILES['file']['tmp_name'] ) ) {
+                $file = !empty( $_FILES['file']['tmp_name'] ) ? dt_recursive_sanitize_array( wp_unslash( $_FILES['file'] ) ) : null;
 
-            if ( in_array( $file["type"], [ "application/xml", "text/xml" ], true ) ) {
-                $all_good_to_go = true;
-            } else {
-                $message = "Please upload an xml file";
+                $tmp_file_name = $file["tmp_name"];
+
+                if ( in_array( $file["type"], [ "application/xml", "text/xml" ], true ) ) {
+                    $all_good_to_go = true;
+                } else {
+                    $message = "Please upload an xml file";
+                }
+                if ( $all_good_to_go && $file["size"] < $max_file_size ) {
+                    $all_good_to_go = true;
+                } else {
+                    $message = "File size is too large.";
+                }
             }
-            if ( $all_good_to_go && $file["size"] < $max_file_size ) {
-                $all_good_to_go = true;
-            } else {
-                $message = "File size is too large.";
+
+            if ( isset( $_POST["rss-feed-url"] ) ) {
+                $rss_feed_url = sanitize_text_field( wp_unslash( $_POST["rss-feed-url"] ) );
+
+                libxml_use_internal_errors(true);
+                $feeds = simplexml_load_file( $rss_feed_url );
+
+                $errors = libxml_get_errors();
+
+
+                if ( !empty( $feeds ) ) {
+                    $all_good_to_go = true;
+                    /* write the data to a file */
+                    $tmp_file = tmpfile();
+
+                    $data = $feeds->asXML();
+
+                    fwrite( $tmp_file, $data );
+
+                    $tmp_file_name = stream_get_meta_data($tmp_file)['uri'];
+                } else {
+                    $invalidurl = true;
+                }
+
             }
 
             if ( $all_good_to_go ) {
                 ob_start();
 
-                ( new WP_Import() )->import( $tmp_file );
+                ( new WP_Import() )->import( $tmp_file_name );
 
                 $import_output = ob_get_contents();
                 ob_end_clean();
 
                 $post_start_date = $prayer_post_importer->find_latest_prayer_fuel_date();
+
             }
+
+            if ( $tmp_file ) {
+                fclose( $tmp_file );
+            }
+
         }
 
         ?>
@@ -290,7 +326,30 @@ class DT_Porch_Admin_Tab_Starter_Content {
                     </tr>
                     <tr>
                         <td>
-                            <input name="file" type="file" accept="application/xml text/xml" required>
+                            <input id="rss-feed-url" name="rss-feed-url" type="text" placeholder="RSS Feed URL">
+                            <?php if ( $invalidurl ): ?>
+
+                                <p>
+                                    The url is invalid
+                                </p>
+
+                            <?php endif; ?>
+
+                            <?php if ( $feeds && empty( $feeds ) ): ?>
+
+                                <p>
+                                    The feed is empty
+                                </p>
+
+                            <?php endif; ?>
+                        </td>
+                        <td>
+                            <label for="rss-reed-url">RSS Feed URL</label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <input name="file" type="file" accept="application/xml text/xml">
                             <?php if ( $message !== "" ): ?>
 
                                 <p>
