f2a6cdb12f64ce2c908809dc8d5546c1e8b87ea8
Nath <nathinabob@gmail.com>
2022-08-31 11:43:05 +0100

    fix: finish refactor of DT_Generic_Porch_...

--- porches/generic/site/rest.php
+++ porches/generic/site/rest.php
@@ -7,8 +7,8 @@ if ( !defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly.
  * @version 1
  */
 
-if ( ! class_exists( 'DT_GenericPorch_Lead_Form' ) ) {
-    class DT_GenericPorch_Lead_Form {
+if ( ! class_exists( 'DT_Generic_Porch_Lead_Form' ) ) {
+    class DT_Generic_Porch_Lead_Form {
 
         private static $_instance = null;
         public static function instance() {
@@ -180,5 +180,5 @@ if ( ! class_exists( 'DT_GenericPorch_Lead_Form' ) ) {
             return $data;
         }
     }
-    DT_GenericPorch_Lead_Form::instance();
+    DT_Generic_Porch_Lead_Form::instance();
 }
