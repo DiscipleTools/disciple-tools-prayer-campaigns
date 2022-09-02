09bec2c071871dc6d721596de12e420371becae9
corsacca <corsacca@gmail.com>
2022-09-02 13:25:59 +0100

    Push Setup wizard to the top of the page.

--- porches/ongoing/dt-ongoing-porch-loader.php
+++ porches/ongoing/dt-ongoing-porch-loader.php
@@ -16,7 +16,7 @@ class DT_Ongoing_Porch_Loader extends DT_Generic_Porch_Loader {
         $ongoing_wizard = [
             'campaign_type' => 'ongoing',
             'porch' => $this->id,
-            'label' => 'Setup Landing page for 24/7 Ongoing Campaign',
+            'label' => '24/7 Ongoing Campaign',
         ];
         $wizard_types[$this->id] = $ongoing_wizard;
 
