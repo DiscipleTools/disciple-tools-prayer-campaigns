c8de1e4d3c00fd3abedc9fafd610ea39ba7690c2
Nath <nathinabob@gmail.com>
2022-09-07 14:50:17 +0100

    feat: make 404 strings tranlateable

--- porches/generic/site/404-body.php
+++ porches/generic/site/404-body.php
@@ -11,7 +11,8 @@
 </style>
 
 <div class="wrapper-404">
-    <h1 class="title-404">Epic 404</h1>
-    <p>It looks like you may have strayed from the narrow path.</p>
-    <p>Try using some of the links in the menu to get back on track.</p>
+    <h1 class="title-404"><?php esc_html_e( 'Epic 404', 'disciple-tools-prayer-campaigns' ) ?></h1>
+    <p><?php esc_html_e( 'It looks like you may have strayed from the narrow path.', 'disciple-tools-prayer-campaigns' ) ?></p>
+    <p><?php esc_html_e( 'Wide is the path that leads to the 404.', 'disciple-tools-prayer-campaigns' ) ?></p>
+    <p><?php esc_html_e( 'Try using some of the links in the menu to get back on track.', 'disciple-tools-prayer-campaigns' ) ?></p>
 </div>
