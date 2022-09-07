2ae9500183c30760217280f093032490c6ebf69f
Nath <nathinabob@gmail.com>
2022-09-07 14:45:11 +0100

    feat: add 404 to /prayer magic links

--- porches/generic/dt-generic-porch.php
+++ porches/generic/dt-generic-porch.php
@@ -58,6 +58,7 @@ class DT_Generic_Porch {
         require_once( 'site/stats.php' );
         require_once( 'site/landing.php' );
         require_once( 'site/rest.php' );
+        require_once( 'site/404.php' );
 
         /* TODO: Porch emails need setting up like the ramadan porch in this file... */
         /* require_once( 'admin/campaigns-config.php' ); */
---
+++ porches/generic/site/404-body.php
@@ -0,0 +1,17 @@
+<style>
+
+    .wrapper-404 {
+        min-height: 300px;
+        text-align: center
+    }
+    .title-404 {
+        font-size: 100px;
+    }
+
+</style>
+
+<div class="wrapper-404">
+    <h1 class="title-404">Epic 404</h1>
+    <p>It looks like you may have strayed from the narrow path.</p>
+    <p>Try using some of the links in the menu to get back on track.</p>
+</div>
