78087a06f404f90a283a9788bfad3229c0b8d627
corsacca <corsacca@gmail.com>
2023-02-06 11:15:21 +0100

    Add campaign name and if ramadan type to mailchimp signup opt-in.

--- parts/components.php
+++ parts/components.php
@@ -21,6 +21,26 @@ function dt_campaign_post( $post ) {
 }
 
 function dt_campaign_sign_up_form() {
+
+    $campaign_name = '';
+    if ( class_exists( 'DT_Porch_Settings' ) ){
+        $porch_fields = DT_Porch_Settings::settings();
+        if ( isset( $porch_fields['title']['value'] ) ){
+            $campaign_name = $porch_fields['title']['value'];
+        }
+    }
+    if ( empty( $campaign_name ) ){
+        $campaign_name = get_the_title();
+    }
+
+    $tags = '7315913';
+    if ( class_exists( 'DT_Porch_Selector' ) ){
+        $selected_porch = DT_Porch_Selector::instance()->get_selected_porch_id();
+        if ( $selected_porch === 'ramadan-porch' ){
+            $tags .= ',11236361';
+        }
+    }
+
     ?>
             <h2><?php esc_html_e( 'Confirm', 'disciple-tools-prayer-campaigns' ); ?></h2>
             <br>
@@ -56,7 +76,8 @@ function dt_campaign_sign_up_form() {
                         <input class="cp-input" type="email" name="email" id="email" placeholder="<?php esc_html_e( 'Email', 'disciple-tools-prayer-campaigns' ); ?>" />
                         <input class="cp-input" type="email" name="EMAIL" id="e2" placeholder="<?php esc_html_e( 'Email', 'disciple-tools-prayer-campaigns' ); ?>" required />
                     </label>
-                    <div hidden="true"><input type="hidden" name="tags" value="7315913"></div>
+                    <div hidden="true"><input type="hidden" name="tags" value="<?php echo esc_html( $tags ); ?>"></div>
+                    <div hidden="true"><input type="hidden" name="CAMPAIGN" value="<?php echo esc_html( $campaign_name ); ?>"></div>
                     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
                     <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d9ad41b66865008d664ac28bf_4df6e5ea4e" tabindex="-1" value=""></div>
                     <div id="mce-responses" class="clear">
