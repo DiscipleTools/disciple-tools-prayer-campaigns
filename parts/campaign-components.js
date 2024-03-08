6afe56a8ef91aa9d67d0eb5d15cf7a2b57673ecd
corsacca <corsacca@gmail.com>
2024-03-08 12:10:40 +0100

    Fix all circles being 100% (#300)

--- parts/campaign-components.js
+++ parts/campaign-components.js
@@ -1184,7 +1184,7 @@ export class cpProgressRing extends LitElement {
       display: block;
       --pi: 3.14159265358979;
       --radius: 50cqi;
-      --stroke-width: max(3px, 5%);
+      --stroke-width: max(3px, 5cqi);
       --normalized-radius: calc(var(--radius) - var(--stroke-width));
       --normalized-radius2: calc(var(--radius) - var(--stroke-width) / 2 + 1);
       --circumference: calc(var(--normalized-radius) * 2 * var(--pi));
