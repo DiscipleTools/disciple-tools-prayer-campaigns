4054bfcfeae199101f7505b2e06812aeed4bbd15
corsacca <corsacca@gmail.com>
2024-03-13 17:27:18 +0100

    Re add prayer circle to individual days. Add action for more content

--- porches/generic/site/archive-body.php
+++ porches/generic/site/archive-body.php
@@ -57,16 +57,25 @@ $today = DT_Campaign_Prayer_Fuel_Post_Type::instance()->get_days_posts( $todays_
                 <?php dt_campaign_user_record_prayed(); ?>
             </div>
 
-            <div class='row'>
-                <?php
-                if ( !isset( $porch_fields['show_prayer_timer']['value'] ) || empty( $porch_fields['show_prayer_timer']['value'] ) || $porch_fields['show_prayer_timer']['value'] !== 'no' ) :
-                    if ( function_exists( 'show_prayer_timer' ) ) : ?>
-                        <div class="col">
-                            <?php echo do_shortcode( "[dt_prayer_timer color='" . PORCH_COLOR_SCHEME_HEX . "' duration='15' lang='" . $lang . "']" ); ?>
-                        </div>
-                    <?php endif;
-                endif; ?>
+            <?php do_action( 'dt_campaigns_after_prayer_fuel', 'after_record_prayed' ); ?>
 
+
+            <div class='row' style="margin-top: 30px">
+                <div class='section-header col'>
+                    <h2 class='section-title wow fadeIn' data-wow-duration='1000ms'
+                        data-wow-delay='0.3s'><?php echo esc_html( __( 'Prayer Circle', 'disciple-tools-prayer-campaigns' ) ); ?></h2>
+                    <hr class="lines wow zoomIn" data-wow-delay="0.3s">
+                    <div>
+                        <?php
+                        if ( !isset( $porch_fields['show_prayer_timer']['value'] ) || empty( $porch_fields['show_prayer_timer']['value'] ) || $porch_fields['show_prayer_timer']['value'] !== 'no' ) :
+                            if ( function_exists( 'show_prayer_timer' ) ) : ?>
+                                <div class="col">
+                                    <?php echo do_shortcode( "[dt_prayer_timer color='" . PORCH_COLOR_SCHEME_HEX . "' duration='15' lang='" . $lang . "']" ); ?>
+                                </div>
+                            <?php endif;
+                        endif; ?>
+                    </div>
+                </div>
             </div>
 
         <?php endif; ?>
