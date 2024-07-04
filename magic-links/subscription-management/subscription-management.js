e6065903c24497284a3cfa184eb66c1f3c5cebf5
corsacca <corsacca@gmail.com>
2024-07-04 15:56:42 +0100

    Subscriber management: Auto extend prayer rec fuel times

--- magic-links/subscription-management/subscription-management.js
+++ magic-links/subscription-management/subscription-management.js
@@ -1,10 +1,54 @@
 jQuery(document).ready(function($){
+  /**
+   * Click the nav bar buttons to show the different panels
+   */
   $('.nav-bar button').on('click', function (){
     $('.nav-bar button').removeClass('active')
     $(this).addClass('active')
     $('.display-panel').hide()
     $(`#${$(this).data('show')}`).show()
+    //remove tab url query parameter
+    var url = new URL(window.location.href)
+    url.searchParams.delete('tab')
+    window.history.replaceState({}, '', url)
   })
+
+  /**
+   * Extend prayer times if needed
+   */
+  //check if the tab url query parameter is "fuel"
+  const url = new URL(window.location.href)
+  if (url.searchParams.get('tab') === 'fuel') {
+    window.campaign_scripts.get_campaign_data().then((data)=>{
+      const recurring_signups = data?.subscriber_info?.my_recurring_signups
+      if ( recurring_signups.length ){
+        recurring_signups.forEach((recurring_sign) => {
+
+          //extend the prayer times if the last time is within the next two months
+          const two_months_from_now = new Date().getTime() + 5184000000
+          const now = new Date().getTime()
+          if (recurring_sign.last > now && recurring_sign.last < two_months_from_now) {
+            let recurring_extend = window.campaign_scripts.build_selected_times_for_recurring(
+              recurring_sign.time, recurring_sign.type,
+              recurring_sign.duration,
+              recurring_sign.week_day || null,
+              recurring_sign.last
+            );
+            recurring_extend.report_id = recurring_sign.report_id
+
+            //filter out existing times
+            let existing_times = window.campaign_data.subscriber_info.my_commitments.filter(c => recurring_sign.report_id===c.recurring_id).map(c => parseInt(c.time_begin))
+
+            recurring_extend.selected_times = recurring_extend.selected_times.filter(c => !existing_times.includes(c.time))
+            window.campaign_scripts.submit_prayer_times(recurring_sign.campaign_id, recurring_extend, 'update_recurring_signup').then(resp => {
+              console.log(resp);
+              //@todo
+            })
+          }
+        })
+      }
+    })
+  }
 })
 
 
