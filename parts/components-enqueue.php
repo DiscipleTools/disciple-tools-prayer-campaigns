4f0f7866d2d0b50e2ff9a5131986373ff281d33a
corsacca <corsacca@gmail.com>
2024-09-10 16:02:00 +0100

    Fix getting current language

--- parts/components-enqueue.php
+++ parts/components-enqueue.php
@@ -35,7 +35,7 @@ function dt_campaigns_register_scripts( $atts, $campaign_id ){
                 'campaign_root' => class_exists( 'DT_Campaign_Landing_Settings' ) ? DT_Campaign_Landing_Settings::get_landing_root_url() : '',
                 'plugin_url' => $plugin_dir_url,
                 'dt_campaigns_is_p4m_news_enabled' => dt_campaigns_is_p4m_news_enabled(),
-                'locale' => dt_campaign_get_current_lang(),
+                'locale' => determine_locale(),
                 'translations' => [
                     'Detected time zone' => __( 'Detected time zone', 'disciple-tools-prayer-campaigns' ),
                     'Select a timezone' => __( 'Select a timezone', 'disciple-tools-prayer-campaigns' ),
